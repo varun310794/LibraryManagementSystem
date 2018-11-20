a475be2e0e000cbb9ada8e18638e3e37bbd50c5d
Varun Reddy Peddaiahgari <varun310794@gmail.com>
2018-11-20 22:21:18 +0000

    added initial routes

--- app.js
+++ app.js
@@ -1,19 +1,33 @@
 var express=require("express");
 var app=express();
 var bodyParser=require("body-parser");
-
+//var mongoose=require("mongoose");
 
 var methodOverride = require("method-override");
-
+app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 
-//mongoose.connect('mongodb://varun:database1@ds155293.mlab.com:55293/knowyourdestination');
-
+//mongoose.connect('mongodb://varun:database1@ds155293.mlab.com:55293/lms', { useNewUrlParser: true });
+// homepage----------------------------------------
 app.get("/", function(req, res){
     res.render("homepage.ejs");
 });
+//new books------------------------------------------
+app.get("/books", function(req, res){
+    res.render("books.ejs");
+});
+//new 
+app.get("/customers", function(req, res){
+    res.render("customers.ejs");
+});
+
+app.get("/employees", function(req, res){
+    res.render("employees.ejs");
+});
+
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("The server has started");
