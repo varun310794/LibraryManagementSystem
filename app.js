8a88e46d722f703e3f3bad97230b002e1c9bdb74
Varun Reddy Peddaiahgari <varun310794@gmail.com>
2018-11-21 05:27:42 +0000

    completed adding and storing books

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express=require("express");
 var app=express();
 var bodyParser=require("body-parser");
-//var mongoose=require("mongoose");
+var mongoose=require("mongoose");
 
 var methodOverride = require("method-override");
 app.set("view engine", "ejs");
@@ -9,20 +9,57 @@ app.use(express.static(__dirname+"/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
 
-//mongoose.connect('mongodb://varun:database1@ds155293.mlab.com:55293/lms', { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/lms",{useNewUrlParser: true});
+
+//schema------------------------------------------
+
+var bookSchema=new mongoose.Schema({
+    name:String,
+    author:String,
+    id:Number
+});
+
+var book=mongoose.model("book",bookSchema);
+
+
 // homepage----------------------------------------
 app.get("/", function(req, res){
     res.render("homepage.ejs");
 });
 //new books------------------------------------------
-app.get("/books", function(req, res){
-    res.render("books.ejs");
+app.get("/newbooks", function(req, res){
+    res.render("newbooks.ejs");
 });
-//new 
+
+app.get("/books", function(req, res){
+    book.find({}, function(err, allbooks){
+        if(err){
+            console.log(err);
+        }else{
+            res.render("books.ejs",{allbooks:allbooks});
+        }
+    });
+});
+
+app.post("/books", function(req, res){
+    var name=req.body.name;
+    var author=req.body.author;
+    var id=req.body.id;
+    var newBook={name:name, author: author, id: id};
+    book.create(newBook,function(err, theBook){
+        if(err){
+            console.log(err);
+        }else{
+            res.redirect("/books");
+        }
+    });
+});
+
+//new customers---------------------------------------- 
 app.get("/customers", function(req, res){
     res.render("customers.ejs");
 });
-
+//new employees---------------------------------------
 app.get("/employees", function(req, res){
     res.render("employees.ejs");
 });
